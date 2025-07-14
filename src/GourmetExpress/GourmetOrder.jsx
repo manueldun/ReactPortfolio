3356055c7206afc90636efb1c9b91a9301eb2b73
Manuel Fernando Dun Jimenez <manueldun@gmail.com>
2025-07-14 16:20:17 -0400

    Added a dialog windows popup to see the current order.

--- src/GourmetExpress/GourmetOrder.jsx
+++ src/GourmetExpress/GourmetOrder.jsx
@@ -3,15 +3,36 @@ import "./GourmetExpress.css";
 import motoLogo from "../assets/moto-logo.png";
 
 function GourmetOrder(props) {
+  const dialogRef = useRef(null);
   let accumTotal = 0;
   for (const meal of props.order) {
     accumTotal += meal.price;
   }
+  function handleCheckout(e) {
+    dialogRef.current.showModal();
+  }
   return (
-    <div id="gourmetOrder">
-      <img src={motoLogo}></img>
-      <h3>Total: {accumTotal}$</h3>
-    </div>
+    <>
+      <div id="gourmetOrder" onClick={handleCheckout}>
+        <img src={motoLogo}></img>
+        <h3>Total: {accumTotal}$</h3>
+      </div>
+      <dialog id="checkoutDialog" ref={dialogRef}>
+        <ul>
+          {props.order.map((meal) => {
+            return <li key={meal.key}>{meal.name + ": " + meal.price + "$"}</li>;
+          })}
+        </ul>
+        <p>total: {accumTotal}$</p>
+        <button
+          onClick={() => {
+            dialogRef.current.close();
+          }}
+        >
+          Accept
+        </button>
+      </dialog>
+    </>
   );
 }
 
